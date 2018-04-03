64f1099430054b73cf0f9dd5f26d45967c710271
scavicchio <scavicchio@nyu.edu>
2018-04-03 18:01:58 -0400

    Update easy-miner.bat
    Now prints hashrate every 30 seconds

--- easy-miner.bat
+++ easy-miner.bat
@@ -6,7 +6,7 @@ set walletInstallPath="C:\Program Files\WTC\walton.exe"
 
 REM ## Other variables ##
 set publicAddressFile=pubaddr.txt
-set delay=5
+set delay=30
 
 setlocal enabledelayedexpansion
 
@@ -18,7 +18,18 @@ set /p id="Enter Etherbase Address: "
 echo !id! > %publicAddressFile%
 )
 
+REM ## Print hashrate every 30 seconds indefinitely ##
 TITLE Walton Hashrate Monitor (Can be closed)
 start start_automated.bat
-timeout %delay%
-%walletInstallPath% attach http://127.0.0.1:8545
+timeout 5 /NOBREAK > NUL
+
+:loop
+cls
+echo Mining to address {specified in %publicAddressFile%}
+type %publicAddressFile%
+echo.
+echo This window will print your hash rate every %delay% seconds.
+echo.
+%walletInstallPath% attach http://127.0.0.1:8545 --exec miner.getHashrate()
+timeout %delay% /NOBREAK > NUL
+goto loop
